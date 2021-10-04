1259c5bb6b5ae77f388039b6d5034e5f81512bc8
Damian <justice.damian96@gmail.com>
2021-10-04 12:25:07 +1100

    initial commit

---
+++ compatabl/src/App.js
@@ -0,0 +1,15 @@
+import react, {useEffect} from 'react';
+import './App.css';
+import Application from './application'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Application />
+    </div>
+  );
+}
+
+export default App;
